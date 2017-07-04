d14c124226740845a9a5374391606a473e7fdb81
naderae <naderabouezze93@gmail.com>
2017-07-04 16:43:35 -0400

    done with part2

---
+++ book_lending.rb
@@ -0,0 +1,107 @@
+class Book
+
+@@on_shelf = []
+@@on_loan =  []
+
+def initialize(title, author, isbn)
+  @title = title
+  @author = author
+  @isbn = isbn
+  @due_date = nil
+  @lent_out = false
+end
+
+
+
+
+def due_date
+  return @due_date
+end
+
+def due_date=(due_date)
+  @due_date = due_date
+end
+
+
+
+def self.create
+  new_book = Book.new(title, author, isbn)
+  @@on_shelf << new_book
+  return new_book
+end
+
+def self.available
+  return @@on_shelf
+end
+
+def self.borrowed
+  return @@on_loan
+end
+
+def self.browse
+  return @@on_shelf.sample
+end
+
+def lent_out?
+   return @lent_out
+end
+
+def self.current_due_date
+  Time.now + 1209600
+end
+
+def borrow
+  if self.lent_out?
+    return false
+  else
+    @due_date = Book.current_due_date
+    @@on_shelf.delete(self)
+    @@on_loan.push(self)
+    return true
+end
+
+def return_to_library
+  if self.lent_out? == false
+    return false
+  else
+    @@on_loan.delete(self)
+    @@on_shelf.push(self)
+    @due_date = nil
+    return true
+end
+
+def self.overdue
+  overdue_books = []
+  @@on_loan.each do |book|
+    if book.due_date < Time.now
+      overdue_books.push(book)
+      return overdue_books
+    end
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
